e99935c5c75270a352a6d851a287fd78cb3f1835
Mario Martinez <mariojaimemartinez27@gmail.com>
2024-10-31 16:41:30 -0600

    Add new cors

--- api/index.js
+++ api/index.js
@@ -9,7 +9,11 @@ const upload = multer();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const openai = new OpenAI({
