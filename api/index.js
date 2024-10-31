67a98fc2c5c65c8b79e06bb0a290e9d9a10be893
Mario Martinez <mariojaimemartinez27@gmail.com>
2024-10-31 17:00:54 -0600

    Update url in cors

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://ara-quizzes.vercel.app",
 };
 
 app.use(cors(corsOptions));
