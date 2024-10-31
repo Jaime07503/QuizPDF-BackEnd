27cd38e63155723027a7b9fd194423c1057327b7
Mario Martinez <mariojaimemartinez27@gmail.com>
2024-10-30 20:18:24 -0600

    Add basic index with Express

--- api/index.js
+++ api/index.js
@@ -1,20 +1,13 @@
 const express = require("express");
-const pdfParse = require("pdf-parse");
-const fs = require("fs");
-const path = require("path");
-const cors = require("cors");
-const OpenAI = require("openai");
-require("dotenv").config();
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
-
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
