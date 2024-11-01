e72fa56cda7f7f89767b737b9603b20dc0f61169
Mario Martinez <mariojaimemartinez27@gmail.com>
2024-10-31 19:38:43 -0600

    Change in cors

--- api/index.js
+++ api/index.js
@@ -10,9 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://ara-quizzes.vercel.app",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -25,49 +26,54 @@ const openai = new OpenAI({
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.post("/upload", upload.single("file"), async (req, res) => {
-  if (!req.file) {
-    return res.status(400).send("No se subió ningún archivo.");
+app.post(
+  "/upload",
+  cors(corsOptions),
+  upload.single("file"),
+  async (req, res) => {
+    if (!req.file) {
+      return res.status(400).send("No se subió ningún archivo.");
+    }
+
+    try {
+      const pdfData = await pdfParse(req.file.buffer);
+      const text = pdfData.text;
+
+      // Lógica para el resumen
+      const summaryResponse = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: `Summarize the following text:\n\n${text}`,
+          },
+        ],
+        max_tokens: 100,
+      });
+
+      const summary = summaryResponse.choices[0].message.content;
+
+      // Lógica para las preguntas
+      const questionsResponse = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: `Generate a three questions and answers from the following text:\n\n${text}`,
+          },
+        ],
+        max_tokens: 300,
+      });
+
+      const questions = questionsResponse.choices[0].message.content;
+
+      res.json({ summary, questions });
+    } catch (error) {
+      console.error("Error en el procesamiento:", error);
+      res.status(500).json({ error: "Error al procesar el archivo." });
+    }
   }
-
-  try {
-    const pdfData = await pdfParse(req.file.buffer);
-    const text = pdfData.text;
-
-    // Lógica para el resumen
-    const summaryResponse = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content: `Summarize the following text:\n\n${text}`,
-        },
-      ],
-      max_tokens: 100,
-    });
-
-    const summary = summaryResponse.choices[0].message.content;
-
-    // Lógica para las preguntas
-    const questionsResponse = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content: `Generate a three questions and answers from the following text:\n\n${text}`,
-        },
-      ],
-      max_tokens: 300,
-    });
-
-    const questions = questionsResponse.choices[0].message.content;
-
-    res.json({ summary, questions });
-  } catch (error) {
-    console.error("Error en el procesamiento:", error);
-    res.status(500).json({ error: "Error al procesar el archivo." });
-  }
-});
+);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
